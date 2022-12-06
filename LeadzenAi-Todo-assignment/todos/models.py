ab5d2bf9784ae36ef49ace98e37c408b3057d814
AbhishekAT77 <83870619+AbhishekAT77@users.noreply.github.com>
2022-12-06 10:47:32 +0530

    Update models.py

--- LeadzenAi-Todo-assignment/todos/models.py
+++ LeadzenAi-Todo-assignment/todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+#The task model contains the fields[Content,data,complete ] "ID" will be primary key generated automatically by Django ORM
 class Task(models.Model):
     content = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
